d00826e9268e45e531f8e24f2d79a7ae9a661738
federicoguevara1021 <117690914+federicoguevara1021@users.noreply.github.com>
2022-11-08 19:36:23 -0500

    Cactus largos
    Implementacion de los cactus largos que es la tarea 2

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -2,26 +2,47 @@ import pygame.time
 
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.utils.constants import LARGE_CACTUS
 
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
+        self.obstacles2 = []
+
 
     def update(self,game):
         if len(self.obstacles) == 0:
             cactus = Cactus(SMALL_CACTUS)
             self.obstacles.append(cactus)
 
+        if len(self.obstacles2) == 0:
+            cactus2 = Cactus(LARGE_CACTUS)
+            self.obstacles2.append(cactus2)
+
+
+
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(1000)
-                game.playing =False
+                game.playing = False
                 break
+
+        for obstacle in self.obstacles2:
+            obstacle.update(game.game_speed,self.obstacles2)
+            if game.player.dino_rect.colliderect(obstacle.rect):
+                pygame.time.delay(700)
+                game.playing = False
+                break
+
 
 
 
     def draw(self,screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+
+        for x in self.obstacles2:
+            x.draw(screen)
+
